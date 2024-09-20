3c8fd6f9259c8dedcd03a9bf1f1713168809071b
a10ghute <A10GHUTE@GMAIL.COM>
2024-09-20 19:10:22 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Smooth scroll for CTA button
+document.querySelector('.cta-button').addEventListener('click', function () {
+    document.querySelector('#products').scrollIntoView({ behavior: 'smooth' });
+});
+
